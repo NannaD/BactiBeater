53603a1e15f65b50dd61dec19f7710c2c8f4483d
NannaD <201706641@post.au.dk>
2020-09-21 10:06:34 +0200

    Adapter added

--- app/src/main/java/Service/BackgroundService.java
+++ app/src/main/java/Service/BackgroundService.java
@@ -1,4 +1,32 @@
 package Service;
 
-public class BackgroundService {
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+import android.util.Log;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import java.util.ArrayList;
+
+import Database.TaskApplication;
+
+public class BackgroundService extends Service {
+    private static final String LOG = "MyBackgroundService";
+
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        Log.d(LOG, "Service running");
+        return super.onStartCommand(intent, flags, startId);
+    }
+
+
+
 }

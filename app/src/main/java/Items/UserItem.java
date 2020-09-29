0d485f2efb39ab3270eaa7d933739ca8d4c5b094
kathrinekahnshille <kathrine.kahns.hille@post.au.dk>
2020-09-29 10:01:18 +0200

    Deleted one line

--- app/src/main/java/Items/UserItem.java
+++ app/src/main/java/Items/UserItem.java
@@ -4,7 +4,6 @@ import java.io.Serializable;
 
 public class UserItem implements Serializable {
 
-    private String nono;
     private String username;
     private String password;
     private boolean canAddUserBool;

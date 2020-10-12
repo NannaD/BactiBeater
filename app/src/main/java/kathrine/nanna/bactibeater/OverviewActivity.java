028a52331a2440b339e00ff6cf436d0505d2bd1d
kathrinekahnshille <kathrine.kahns.hille@post.au.dk>
2020-10-12 17:18:32 +0200

    PieChart functionality in OverviewActivity class (start of)

--- app/src/main/java/kathrine/nanna/bactibeater/OverviewActivity.java
+++ app/src/main/java/kathrine/nanna/bactibeater/OverviewActivity.java
@@ -7,19 +7,31 @@ import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
 import android.content.ServiceConnection;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.os.IBinder;
 import android.view.View;
 import android.widget.Button;
 
+import com.github.mikephil.charting.charts.PieChart;
+import com.github.mikephil.charting.data.PieData;
+import com.github.mikephil.charting.data.PieDataSet;
+import com.github.mikephil.charting.data.PieEntry;
+import com.github.mikephil.charting.utils.ColorTemplate;
+
+import java.util.ArrayList;
+import java.util.List;
+
 import Service.BackgroundService;
 
 public class OverviewActivity extends AppCompatActivity {
 
-    //Buttons
+    //Buttons and widgets
     private Button goBackB;
     private Button exitB;
     private Button goToChooseActivity;
+    private PieChart overviewPieChart;
+
     private BackgroundService bService;
     private boolean bound;
 
@@ -32,12 +44,13 @@ public class OverviewActivity extends AppCompatActivity {
         goBackB = findViewById(R.id.goBackB);
         exitB = findViewById(R.id.exitB);
         goToChooseActivity = findViewById(R.id.specificDataB);
+        overviewPieChart = findViewById(R.id.overviewPieChart);
 
+        //Setup backgroundservice connection
         setupConnectionToService();
         bindToService();
 
-        //Button functionality
-
+        //Button functionality for navigation
         goToChooseActivity.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -68,6 +81,25 @@ public class OverviewActivity extends AppCompatActivity {
             }
         });
 
+        updateOverviewPieChart();
+    }
+
+    public void updateOverviewPieChart(){
+        ArrayList<PieEntry> visitorsSanitationToday = new ArrayList<>();
+        visitorsSanitationToday.add(new PieEntry(33, "Sanitized"));
+        visitorsSanitationToday.add(new PieEntry(44, "Failed to sanitize"));
+
+        PieDataSet pieDataSet = new PieDataSet(visitorsSanitationToday, "Visitors and sanitation");
+        pieDataSet.setColors(ColorTemplate.COLORFUL_COLORS);
+        pieDataSet.setValueTextColor(Color.BLACK);
+        pieDataSet.setValueTextSize(16f);
+
+        PieData pieData = new PieData(pieDataSet);
+
+        overviewPieChart.setData(pieData);
+        overviewPieChart.getDescription().setEnabled(false);
+        overviewPieChart.setCenterText("Visitors and sanitation");
+        overviewPieChart.animate();
     }
 
     //Recieving broadcasts
